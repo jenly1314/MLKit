747ef55eea566b832931320fbc1f36c7f395ba32
Jenly <jenly1314@gmail.com>
2021-04-06 18:55:13 +0800

    Update MLKit

--- README.md
+++ README.md
@@ -10,3 +10,63 @@
 ML Kit是一个能够将谷歌专业的机器学习知识带到应用中的极其简单易用的封装包。无论您是否有机器学习的经验，您都可以在几行代码中实现您想要的功能。甚至，您无需对神经网络或者模型优化有多深入的了解，也能完成您想要做的事情。
 基于现有的API您可以很轻松的实现文字识别、条码识别、图像标记、人脸检测、对象检测等功能；另一方面，如果您是一位经验丰富的ML开发人员，ML kit甚至提供了便利的API，可帮助您在移动应用中使用自定义的TensorFlow Lit模型。
 
+
+## 各Module相关说明
+
+### [app](app)
+
+示例App：主要用于提供MLKit各个子库的演示效果
+
+### [mlkit-camera-core](mlkit-camera-core)
+
+Camera：为各个子库提供相机预览分析的核心库
+
+> 参见[CameraX](https://developer.android.google.cn/training/camerax)
+
+### [mlkit-barcode-scanning](mlkit-barcode-scanning)
+
+条码扫描：通过分析图像能够识别条码的内容信息
+
+> 参见[barcode-scanning](https://developers.google.cn/ml-kit/vision/barcode-scanning)
+
+### [mlkit-face-detection](mlkit-face-detection)
+
+人脸检测：通过分析图像能够检测到人脸和分析人脸轮廓关键点信息
+
+> 参见[face-detection](https://developers.google.cn/ml-kit/vision/face-detection)
+
+### [mlkit-image-labeling](mlkit-image-labeling)
+
+图像标记：通过分析图像能够标记一般对象、场所、动物种类、产品等
+
+> 参见[image-labeling](https://developers.google.cn/ml-kit/vision/image-labeling)
+
+### [mlkit-object-detection](mlkit-object-detection)
+
+对象检测：通过分析图像能够检测出图像中的对象的位置信息（一张图最多可以检测五个对象）
+
+> 参见[object-detection](https://developers.google.cn/ml-kit/vision/object-detection)
+
+### [mlkit-pose-detection](mlkit-pose-detection)
+
+Pose检测：通过分析图像能够检测人物摆姿势的关键点信息
+
+> 参见[pose-detection](https://developers.google.cn/ml-kit/vision/pose-detection)
+
+### [mlkit-pose-detection-accurate](mlkit-pose-detection-accurate)
+
+Pose检测：通过分析图像能够检测人物摆姿势的关键点信息（精确版，依赖包也更大）
+
+> 参见[pose-detection](https://developers.google.cn/ml-kit/vision/pose-detection)
+
+### [mlkit-segmentation-selfie](mlkit-segmentation-selfie)
+
+自拍分割：通过分析图像能够将自拍照的人物特征进行分割
+
+> 参见[selfie-segmentation](https://developers.google.cn/ml-kit/vision/selfie-segmentation)
+
+### [mlkit-text-recognition](mlkit-text-recognition)
+
+文字识别：识别图像中的文字信息
+
+> 参见[text-recognition](https://developers.google.cn/ml-kit/vision/text-recognition)
--- app/src/main/java/com/king/mlkit/vision/app/segmentation/SelfieSegmentationActivity.kt
+++ app/src/main/java/com/king/mlkit/vision/app/segmentation/SelfieSegmentationActivity.kt
@@ -1,8 +1,12 @@
 package com.king.mlkit.vision.app.segmentation
 
+import android.graphics.*
+import android.widget.ImageView
+import androidx.annotation.ColorInt
 import com.google.mlkit.vision.segmentation.SegmentationMask
 import com.king.app.dialog.AppDialog
 import com.king.app.dialog.AppDialogConfig
+import com.king.mlkit.vision.app.R
 import com.king.mlkit.vision.camera.AnalyzeResult
 import com.king.mlkit.vision.segmentation.SegmentationCameraScanActivity
 
@@ -14,19 +18,70 @@ class SelfieSegmentationActivity : SegmentationCameraScanActivity() {
     override fun onScanResultCallback(result: AnalyzeResult<SegmentationMask>) {
         cameraScan.setAnalyzeImage(false)
 
-        val mask = result.result.buffer
-        val maskWidth = result.result.width
-        val maskHeight = result.result.height
-
-        val config = AppDialogConfig(this)
-        config.setContent("${mask.float}")
-            .setOnClickOk {
-                AppDialog.INSTANCE.dismissDialog()
-                cameraScan.setAnalyzeImage(true)
-            }.setOnClickCancel {
-                AppDialog.INSTANCE.dismissDialog()
-                finish()
-            }
+        val config = AppDialogConfig(this, R.layout.result_dialog)
+        config.setOnClickOk {
+            AppDialog.INSTANCE.dismissDialog()
+            cameraScan.setAnalyzeImage(true)
+        }.setOnClickCancel {
+            AppDialog.INSTANCE.dismissDialog()
+            finish()
+        }
+        val imageView = config.getView<ImageView>(R.id.ivDialogContent)
+        imageView.setImageBitmap(processBitmap(result))
         AppDialog.INSTANCE.showDialog(config)
+    }
+
+    private fun processBitmap(result: AnalyzeResult<SegmentationMask>): Bitmap{
+        val bitmap = result.bitmap
+        val mask = result.result
+        val maskWidth = mask.width
+        val maskHeight = mask.height
+
+        val isRawSizeMaskEnabled = maskWidth != bitmap.width || maskHeight != bitmap.height
+        val scaleX = bitmap.width * 1f / maskWidth
+        val scaleY = bitmap.height * 1f / maskHeight
+
+
+        val resultBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        val maskBitmap = Bitmap.createBitmap(maskColorsFromByteBuffer(mask), maskWidth, maskHeight, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(resultBitmap)
+        val paint = Paint()
+        //绘制原始图片
+        canvas.drawBitmap(bitmap, 0f, 0f, null)
+        paint.strokeWidth = 6f
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.RED
+        val matrix = Matrix()
+        if (isRawSizeMaskEnabled) {
+            matrix.preScale(scaleX, scaleY)
+        }
+        //绘制蒙版图层
+        canvas.drawBitmap(maskBitmap, matrix, null)
+        bitmap.recycle()
+
+        canvas.save()
+        canvas.restore()
+
+        return resultBitmap
+    }
+
+    @ColorInt
+    private fun maskColorsFromByteBuffer(mask: SegmentationMask): IntArray {
+        @ColorInt val colors =
+            IntArray(mask.width * mask.height)
+        for (i in 0 until mask.width * mask.height) {
+            val backgroundLikelihood = 1 - mask.buffer.float
+            if (backgroundLikelihood > 0.9) {
+                colors[i] = Color.argb(128, 255, 0, 255)
+            } else if (backgroundLikelihood > 0.2) {
+                // Linear interpolation to make sure when backgroundLikelihood is 0.2, the alpha is 0 and
+                // when backgroundLikelihood is 0.9, the alpha is 128.
+                // +0.5 to round the float value to the nearest int.
+                val alpha = (182.9 * backgroundLikelihood - 36.6 + 0.5).toInt()
+                colors[i] = Color.argb(alpha, 255, 0, 255)
+            }
+        }
+        return colors
     }
 }

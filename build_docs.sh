9e3ae297523c8e362a4149c83a7618302200d9c2
Jenly <jenly1314@gmail.com>
2024-08-18 23:18:18 +0800

    docs

--- build_docs.sh
+++ build_docs.sh
@@ -12,13 +12,13 @@ mv build/dokka/htmlMultiModule/* docs/api
 GITHUB_URL=https://github.com/jenly1314/MLKit/
 echo $GITHUB_URL
 sed "/<!-- end -->/q" README.md > docs/index.md
-sed -i "s|app/src/main/ic_launcher-web.png|ic_logo.png|g" docs/index.md
+# sed -i "s|app/src/main/ic_launcher-web.png|ic_logo.png|g" docs/index.md
 sed -i "s|](app|](${GITHUB_URL}blob/master/app|g" docs/index.md
 sed -i "s|](mlkit|](${GITHUB_URL}blob/master/mlkit|g" docs/index.md
 cat CHANGELOG.md | grep -v '## 版本日志' > docs/changelog.md
 
 cp GIF.gif docs/GIF.gif
-cp app/src/main/ic_launcher-web.png docs/ic_logo.png
+# cp app/src/main/ic_launcher-web.png docs/ic_logo.png
 
 # Build the site locally
 mkdocs build
